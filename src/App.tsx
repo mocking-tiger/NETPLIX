9b10d24f53e7cf54d16507842fea43024b9a926e
Park JunYong <dyden119@gmail.com>
2025-01-12 14:10:39 +0900

    chore: 임시 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import Home from "./routes/Home";
+import Tv from "./routes/Tv";
+import Search from "./routes/Search";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+
 function App() {
-  return <div>초기화</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
